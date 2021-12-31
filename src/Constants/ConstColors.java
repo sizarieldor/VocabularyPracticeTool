1311a2f77c75a10eb713a8f30acdd21a408927b5
sizarieldor <77972735+sizarieldor@users.noreply.github.com>
2021-12-31 19:07:36 +0200

    Delete src directory

--- src/Constants/ConstColors.java
+++
@@ -1,9 +0,0 @@
-package Constants;
-
-import java.awt.*;
-
-public class ConstColors {
-    public static final Color COLOR_LIGHT_BLUEISH = new Color(180,220,200);
-    public static final Color COLOR_LIGHT_GREENISH = new Color(180,240,190);
-    public static final Color COLOR_LIGHT_REDDISH = new Color(240,200,190);
-}
